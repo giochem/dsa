426664fee67f2843cb14aae0093be5747f3217c3
Xuân Trường <96410595+giochem@users.noreply.github.com>
2025-09-12 15:43:04 +0700

    refactor project structure

--- java/2785.sort-vowels-in-string/Solution.java
+++ java/2785.sort-vowels-in-string/Solution.java
@@ -27,9 +27,7 @@ class Solution {
         }
         return sb.toString();
     }
-}
 
-class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println(sol.sortVowels("lEetcOde"));
